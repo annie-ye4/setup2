6b8cc2e0ec8e52573990a73696a8a298820c2077
annie-ye4 <113400571+annie-ye4@users.noreply.github.com>
2024-08-30 14:42:13 -0400

    using checkstyle

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+/**
+ * Main class.
+ */
+public class Main {
+
+  public static void main(String[] args) {
+         boolean ANSWER = getTruth();
+    if (ANSWER) {
+      System.out.println("cs32 is the best class!");
+    }
+  }
+
+  public static boolean getTruth() {
+    return true;
+  }
+}
